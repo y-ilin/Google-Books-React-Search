4c491e620c59dc9f8399fd02edd59ead2ac38960
Yi Lin Lee <yilinlee19@gmail.com>
2020-10-04 23:19:46 +1100

    clean up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from "./logo.svg";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Nav from "./components/Nav";
