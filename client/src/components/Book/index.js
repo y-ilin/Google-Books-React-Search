0b7d7d2c256ffe3cfc0b0570f27376ede3f84308
Yi Lin Lee <yilinlee19@gmail.com>
2020-10-04 23:20:03 +1100

    add Book component

---
+++ client/src/components/Book/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+import Button from "react-bootstrap/Button";
+
+function Book(props) {
+  return (
+    <li>
+        <p className="title">{props.title}</p>
+        <p className="authors">By: {props.authors ? props.authors.join(", ") : "Unknown"}</p>
+        <div className="bookImgDesc">
+            <img src={props.image} alt="book cover thumbnail" />
+            <p className="description">{props.description}</p>
+        </div>
+        <div className="bookButtons">
+            <Button href={props.infoLink} target="blank" variant="primary">View</Button>
+            {props.saveBook ? 
+                <Button onClick={() => props.saveBook(props.bookInfo)}>Save</Button> :
+                <Button onClick={() => props.deleteBook(props.book._id)}>Delete</Button>
+                    }
+            {/* <Button onClick={() => props.saveBook(props.bookInfo)}>Save</Button> */}
+        </div>
+    </li>
+)
+}
+
+export default Book;
